88b81dc3ddd1ff7e32399a65631fc8d7722b0b3f
Gynt <Gynt@users.noreply.github.com>
2025-02-03 22:59:58 +0100

    simple class

---
+++ reccmp-tests.cpp
@@ -0,0 +1,30 @@
+﻿// reccmp-tests.cpp : Defines the entry point for the application.
+//
+
+#include "reccmp-tests.h"
+
+using namespace std;
+
+class A {
+public:
+
+	int a = 0;
+
+	A() {
+
+	}
+
+	// FUNCTION RECCMP-TESTS 0x401000
+	void method1(int b) {
+		this->a += b;
+	}
+};
+
+static A STATIC_A;
+
+int main(int argc, char**argv)
+{
+	cout << "Hello CMake." << endl;
+	STATIC_A.method1(argc);
+	return 0;
+}
